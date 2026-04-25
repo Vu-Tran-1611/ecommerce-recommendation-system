65a8807639a0473419454bd03dfd2f60df19ca5d
tran my <tranmy@Mac.attwifi.manager>
2026-04-25 18:36:05 -0400

    Fix Response Schema

--- app/api/recommend.py
+++ app/api/recommend.py
@@ -37,7 +37,7 @@ def recommend_recent(request: UserRecentRecommendationRequest):
         )
         return UserRecommendationResponse(
             model_name = request.model_name,
-            recommendations = recommendations[0],
+            recommendations = recommendations,
             # precision = recommendations[1],
             # recall = recommendations[2], 
             # hits = recommendations[3],
